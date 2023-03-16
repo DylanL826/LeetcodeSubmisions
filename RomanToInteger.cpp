60ecd917aef61a8f55b66c84a885b924c7a7ac49
Dylan Lewis <dylanl826@gmail.com>
2023-03-15 22:43:22 -0500

    Innit commit

---
+++ RomanToInteger.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/roman-to-integer/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    // Convert roman numeral to integer and return it.
+    int romanToInt(string s) {
+        int num = 0;
+        int numeralSize = 1;
+        
+        for (int i = 0; i < s.length(); i++)
+        {
+            
+        }
+        return num;
+    }
+};
+
+
+int main(){
+
+    string numeral = "XXII";
+    Solution* obj;
+    obj->romanToInt(numeral);    
+    return 0;
+}
