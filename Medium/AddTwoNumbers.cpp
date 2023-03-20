04f33adaadf6374d67b1d6930f3382ece0113aa4
Dylan Lewis <dylanl826@gmail.com>
2023-03-20 13:22:45 -0500

    First working solution.

--- Medium/AddTwoNumbers.cpp
+++ Medium/AddTwoNumbers.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 #include<stack>
 #include<vector>
+#include<cmath>
 
 using namespace std;
 
@@ -19,17 +20,19 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int numDigits = 0;
-        ListNode* solutionNode = new ListNode();
         // Initialize numbers represented by linkedLists 1 & 2.
         stack<int> listNums;
         int num1 = 0;
         int num2 = 0;
         // Reverse number sequence w/ stack.
+        // TODO: Try adding one digit at a time so you could iterate both...
+        // ... LL's in one loop & not need to store the values into a stack.
         while (l1 != nullptr)
         {
             listNums.push(l1->val);
             l1 = l1->next;
         }
+        numDigits = listNums.size();
         while(!listNums.empty()){
             num1 += listNums.top();
             // TODO: how to not do this on last iteration. 
@@ -48,6 +51,21 @@ public:
             listNums.pop();
         }
         int num3 = (num1 + num2) / 10;
+        // Initialize head.
+        ListNode* solutionNode = new ListNode();
+        // Initialize iterator node variable.
+        ListNode* node = solutionNode;
+        int temp;
+        for (int i = 0; i < numDigits; i++){  
+            // TODO: Check speed & memory improvement after removing
+            // ... temp variable          
+            temp = num3 % int(pow(10,i+1));
+            temp = temp / int(pow(10, i));            
+            node->val = temp;
+            // TODO: Cheaper way to do this than checking every iteration?
+            (i<numDigits-1) ? node->next = new ListNode(): node->next = nullptr;            
+            node = node->next;
+        }        
         return solutionNode;
     }
 };
@@ -71,15 +89,16 @@ int main(){
     Solution* obj = new Solution();
     ListNode* solNode = new ListNode();
     solNode = obj->addTwoNumbers(node1a, node2a);
-    vector<int> solutionV;
+    stack<int> solutionS;
     while (solNode != nullptr)
     {
-        solutionV.push_back(solNode->val);
+        solutionS.push(solNode->val);
         solNode = solNode->next;
     }
-    for (int i = 0; i < solutionV.size(); i++)
+    while (!solutionS.empty())
     {
-        cout << solutionV[i] << endl;
+        cout << solutionS.top() << endl;
+        solutionS.pop();
     }
     
     return 0;
