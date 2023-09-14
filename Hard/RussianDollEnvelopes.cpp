6b287b6827067cc887b32d03c93d460810389d0e
Dylan Lewis <dylanl826@gmail.com>
2023-09-14 16:30:31 -0500

    Init Commit russian doll envelopes DP problem.

---
+++ Hard/RussianDollEnvelopes.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/russian-doll-envelopes/description/LIS/
+// Only Solution class definition is submitted to leetcode.
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        // Get largest x & y dimensions across all the envelopes.
+        int maxX, maxY = 0;
+        for (auto i = 0; i < envelopes.size(); i++)
+        {
+           
+        }
+        
+        // Construct e matrix, e(i,j) = envelope exists? 1 : 0
+        // for (auto i = 0; i < count; i++)
+        // {
+        //     /* code */
+        // }
+        
+        // Initialize M(maxX,maxY) matrix, tracks maximum amount of envelopes 
+        // for each pair of dimensions.
+        // for i,
+        //  for j,
+        // if()
+    }
+};
