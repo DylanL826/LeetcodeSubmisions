d61617bc50a9c3975df6abde33ca0538001dd68b
Dylan Lewis <dylanl826@gmail.com>
2023-09-15 16:07:08 -0500

    Get largest x & y dimensions, and Initialize exist matrix.

--- Hard/RussianDollEnvelopes.cpp
+++ Hard/RussianDollEnvelopes.cpp
@@ -13,9 +13,25 @@ public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         // Get largest x & y dimensions across all the envelopes.
         int maxX, maxY = 0;
+        for (auto i = 0; i < envelopes.size(); i++){
+            if(envelopes[i][0] > maxX){
+                maxX = envelopes[i][0];
+            }
+            if(envelopes[i][1] > maxY){
+                maxY = envelopes[i][1];
+            }
+        }
+        
+        // Declare exists matrix 
+        int** exists = new int*[maxX];
+        for (auto i = 0; i < maxX; i++)
+        {
+            exists[i] = new int[maxY]{0};
+        }
+        // Assign 1 to (x,y) dimensions corresponding to envelope in envelopes.
         for (auto i = 0; i < envelopes.size(); i++)
         {
-           
+            exists[envelopes[i][0]][envelopes[i][1]] = 1;
         }
         
         // Construct e matrix, e(i,j) = envelope exists? 1 : 0
@@ -29,5 +45,25 @@ public:
         // for i,
         //  for j,
         // if()
+
+        // Deallocate dynamic matrices.
+        for (auto i = 0; i < maxX; i++)
+        {
+            delete[] exists[i];
+        }
+        delete[] exists;
+        return -1;
     }
 };
+
+int main() {
+    Solution* obj;
+    int num = -1;
+    vector<vector<int>> envelopes;
+    envelopes.push_back(vector<int>{5,4});
+    envelopes.push_back(vector<int>{6,4});
+    envelopes.push_back(vector<int>{6,7});
+    envelopes.push_back(vector<int>{2,3});
+    num = obj->maxEnvelopes(envelopes);
+    return 0;
+}
