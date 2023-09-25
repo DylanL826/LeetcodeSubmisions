cc37c250835500fc4269d75d2d8ef266a2634715
Dylan Lewis <dylanl826@gmail.com>
2023-09-25 15:57:49 -0500

    Implement (previously) working solution. Seg fault error in M initialization.

--- Hard/RussianDollEnvelopes.cpp
+++ Hard/RussianDollEnvelopes.cpp
@@ -28,7 +28,7 @@ public:
         }
         
         // Declare 'exists' and 'M' matrices
-        for (auto i = 0; i < maxX+1; i++)
+        for (auto i = 0; i <= maxX; i++)
         {
             exists[i] = new int[maxY+1];
             M[i] = new int[maxY+1];
@@ -44,35 +44,37 @@ public:
             
         }
         // Update exists & M matrices at indeces where an envelope exists.
-        for (auto i = 1; i < envelopes.size(); i++)
+        for (auto i = 0; i < envelopes.size(); i++)
         {
             exists[envelopes[i][0]][envelopes[i][1]] = 1;
             //M[envelopes[i][0]][envelopes[i][1]] = 1;
         }
-        
+        printGrid(exists, maxX, maxY);
         // At each element of M, propagate the cells solution to (i+1) & (j+1)
         // for each pair of dimensions.
-        for (auto i = 1; i <= maxX; ++i)
+        for (int i = 1; i <= maxX; ++i)
         {
-            for (auto j = 1; j <= maxY; ++j)
+            for (int j = 1; j <= maxY; ++j)
             {
+                // Update cells where an envelope exists.
+                M[i][j] += exists[i][j];
                 // Check if not on maxX edge of matrix before indexing (x+1) neighbor.
                 if(i != maxX){
                     // Propagate this cells solution if it's better than 
                     //its neighbors solution
-                    if(exists[i][j] + M[i][j] > exists[i+1][j] + M[i+1][j]){
-                        M[i+1][j] = M[i][j] + exists[i][j];
+                    if(M[i][j] >  M[i+1][j]){
+                        M[i+1][j] = M[i][j];
                     }
                 }
                 // Check if not on MaxY edge of matrix before indexing (y+1) neighbor.
                 if(i != maxY){
                     // Propagate this cells solution if it's better than 
                     //its neighbors solution
-                    if(exists[i][j] + M[i][j] > exists[i][j+1] + M[i][j+1]){
-                        M[i][j+1] = M[i][j] + exists[i][j];
+                    if(M[i][j] > M[i][j+1]){
+                        M[i][j+1] = M[i][j];
                     }
                 }
-                //if(exists[i][j] + M[i][j] > exists[i][j+1] + M[i][j+1])
+                printGrid(M, maxX, maxY);
             }
             
         }
@@ -88,6 +90,43 @@ public:
         delete[] exists;
         return M[maxX][maxY];
     }
+
+    void printGrid(int** M, int maxX, int maxY){
+        cout << endl;
+        for (auto i = 0; i <= maxX; i++)
+        {
+            for (auto j = 0; j <= maxY; j++)
+            {
+                cout << " " << M[i][j];
+            }
+            cout << endl;
+        }
+        
+    }
+
+    // Return optimal sequence of envelopes 
+    vector<vector<int>> getEnvelopeSeq(int** M, int maxX, int maxY){
+        int i = maxX;
+        int j = maxY;
+        vector<vector<int>> sequence;
+        sequence.push_back(vector<int>{i,j});
+        while(i + j > 0){
+            // If cur val != neighbor vals, record envelope
+            if(i > 0){
+                if(M[i][j] > M[i-1][j]){
+
+                }
+            }
+            // If not on either edge, compare top and left neighbors
+            if(i * j > 0){
+                // If left neig. > top neig. move left
+                if(M[i-1][j] > M[i][j-1]){
+                    i--;
+                }
+            }
+        }
+        return sequence;
+    }
 };
 
 int main() {
@@ -98,6 +137,10 @@ int main() {
     envelopes.push_back(vector<int>{6,4});
     envelopes.push_back(vector<int>{6,7});
     envelopes.push_back(vector<int>{2,3});
+    envelopes.push_back(vector<int>{3,2});
+    envelopes.push_back(vector<int>{10,7});
+    envelopes.push_back(vector<int>{15,9});
+    envelopes.push_back(vector<int>{12,8});
     num = obj->maxEnvelopes(envelopes);
     cout << num << endl;
     return 0;
