cec72ad2aece3fb6678f94879bf0f5c1501c97f7
Dylan Lewis <dylanl826@gmail.com>
2023-10-02 14:44:23 -0500

    Implement envelope 'pointer' matrix for dimensions largest pair<int,int>** to track dimensions of envelope at each sub-solution.

--- Hard/RussianDollEnvelopes.cpp
+++ Hard/RussianDollEnvelopes.cpp
@@ -14,6 +14,12 @@ public:
         // Get largest x & y dimensions across all the envelopes.
         int maxX = 0;
         int maxY = 0;
+        // Binary matrix if an envelope exists at [i][j] dimensions.
+        int** exists;
+        // Matrix of optimal solutions. 
+        int** M;
+        // Dimensions of largest envelope in solution sequence at [i][j] dimensions.
+        pair<int,int>** largest;
         
         for (auto i = 0; i < envelopes.size(); i++){
             if(envelopes[i][0] > maxX){
@@ -24,15 +30,17 @@ public:
             }
         }
         // exists[i][j] = 1 if an envelope exists of dimension (i,j), else 0.
-        int** exists = new int*[maxX+1];
+        exists = new int*[maxX+1];
         // M[i][j] = max number of envelopes you can fit in dimensions (i,j)
-        int** M = new int*[maxX+1];
-        
+        M = new int*[maxX+1];
+        largest = new pair<int,int>*[maxX+1];
+
         // Declare 'exists' and 'M' matrices
         for (auto i = 0; i <= maxX; i++)
         {
             exists[i] = new int[maxY+1];
             M[i] = new int[maxY+1];
+            largest[i] = new pair<int,int>[maxX+1];
         }
         // Assign 0 to all elements in 'exists' and 'M' 
         for (auto i = 0; i <= maxX; i++)
@@ -41,6 +49,8 @@ public:
             {
                 exists[i][j] = 0;
                 M[i][j] = 0;
+                largest[i][j].first = 0;
+                largest[i][j].second = 0;
             }
             
         }
@@ -56,14 +66,34 @@ public:
         {
             for (int j = 1; j <= maxY; ++j)
             {
-                // Update cells where an envelope exists.
-                M[i][j] += exists[i][j];
+
+                // If an envelope exists @ (i,j) and it will contain largest...
+                // ...envelope of the sequence, update this cell.
+                if(exists[i][j] == 1){
+                    // Needs to be M[i][j] comparison here before comparing 
+                    // largest[i][j] to i & j.
+                    /*
+                        Logic is by only chde
+                    */
+                    if(largest[i][j].first < i && largest[i][j].second < j){
+                        // Update largest envelope in solution sequence to this one.
+                        largest[i][j].first = i;
+                        largest[i][j].second = j;
+
+                        // Update max num envlopes.
+                        M[i][j] += exists[i][j];
+                    }
+
+                }
+                // Propagate solution
                 // Check if not on maxX edge of matrix before indexing (x+1) neighbor.
                 if(i != maxX){
-                    // Propagate this cells solution if it's better than 
+                    // Propagate this cells solution if it's better than
                     //its neighbors solution
                     if(M[i][j] >  M[i+1][j]){
                         M[i+1][j] = M[i][j];
+                        largest[i+1][j].first = largest[i][j].first;
+                        largest[i+1][j].second = largest[i][j].second;
                     }
                 }
                 // Check if not on MaxY edge of matrix before indexing (y+1) neighbor.
@@ -72,23 +102,30 @@ public:
                     //its neighbors solution
                     if(M[i][j] > M[i][j+1]){
                         M[i][j+1] = M[i][j];
+                        largest[i][j+1].first = largest[i][j].first;
+                        largest[i][j+1].second = largest[i][j].second;
                     }
                 }
-                //printGrid(M, maxX, maxY);
+                printGrid(M, maxX, maxY);
+                //printEnvGrid(largest, maxX, maxY);
             }
             
         }
         // Get envelope sequence from backtracing. 
         // if(M[i-1][j] + exists[i-1][j] == M[i][j]) // (x-1) neighbor in optimal path.
 
-
+        int solution = M[maxX][maxY];
         // Deallocate dynamic matrices.
         for (auto i = 0; i < maxX; i++)
         {
             delete[] exists[i];
+            delete[] M[i];
+            delete[] largest[i];
         }
         delete[] exists;
-        return M[maxX][maxY];
+        delete[] M;
+        delete[] largest;
+        return solution;
     }
 
     // Print values of dynamic matrix
@@ -105,6 +142,19 @@ public:
         
     }
 
+    void printEnvGrid(pair<int,int>** largest, int maxX, int maxY){
+        cout << endl;
+        for (auto i = 0; i <= maxX; ++i)
+        {
+            for (auto j = 0; j <= maxY; ++j)
+            {
+                cout << "  (" << largest[i][j].first << "," << largest[i][j].second << ")";
+            }
+            cout << endl;
+        }
+        
+    }
+    
     // Return optimal sequence of envelopes 
     vector<vector<int>> getEnvelopeSeq(int** M, int maxX, int maxY){
         int i = maxX;
@@ -134,20 +184,11 @@ int main() {
     Solution* obj = new Solution();
     int num = -1;
     vector<vector<int>> envelopes;
-    envelopes.push_back(vector<int>{5,4});
-    envelopes.push_back(vector<int>{6,4});
-    envelopes.push_back(vector<int>{6,7});
-    envelopes.push_back(vector<int>{2,3});
-    envelopes.push_back(vector<int>{3,2});
-    envelopes.push_back(vector<int>{10,7});
-    envelopes.push_back(vector<int>{15,9});
-    envelopes.push_back(vector<int>{12,8});
-    envelopes.push_back(vector<int>{1,2});
-    envelopes.push_back(vector<int>{1,2});
-    envelopes.push_back(vector<int>{4,8});
-    envelopes.push_back(vector<int>{4,8});
-    envelopes.push_back(vector<int>{4,150});
-    envelopes.push_back(vector<int>{200,150});
+    envelopes.push_back(vector<int>{6, 10});
+    envelopes.push_back(vector<int>{11, 14});
+    envelopes.push_back(vector<int>{6, 1});
+    envelopes.push_back(vector<int>{16, 14});
+    envelopes.push_back(vector<int>{13, 2});
     num = obj->maxEnvelopes(envelopes);
     cout << num << endl;
     return 0;
