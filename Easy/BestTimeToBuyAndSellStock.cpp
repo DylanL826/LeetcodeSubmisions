dac869dd78318e3bbcaa5971e25b207c91d850e7
Dylan Lewis <dylanl826@gmail.com>
2023-10-19 14:44:40 -0500

    First brute force solution. (Too slow).

---
+++ Easy/BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0, tempProfit = 0;
+        for (auto i = 0; i < prices.size()-1; i++)
+        {
+            for (auto j = i+1; j < prices.size(); j++)
+            {
+                tempProfit = prices[j] - prices[i];
+                if(tempProfit > profit){
+                    profit = tempProfit;
+                }
+            }
+        }
+        return profit;    
+    }
+};
+
+
+int main() {
+    Solution sol = Solution();
+    vector<int> test1({7,1,5,3,6,4});
+    vector<int> test2({7,6,4,3,1});
+    cout << sol.maxProfit(test2) << endl;
+    return 0;
+}
