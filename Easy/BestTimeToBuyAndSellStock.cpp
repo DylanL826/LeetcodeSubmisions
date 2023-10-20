03f7c8bc0d8cc8f0e5be0af95e81db5c3e777ead
Dylan Lewis <dylanl826@gmail.com>
2023-10-20 14:44:32 -0500

    Implemented greedy solution Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/39039/sharing-my-simple-and-clear-c-solution/ This solution has shown me I don't understand greedy algorithms as well as I think I do.

--- Easy/BestTimeToBuyAndSellStock.cpp
+++ Easy/BestTimeToBuyAndSellStock.cpp
@@ -1,25 +1,27 @@
+// Greedy algo solution:
+/*
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/solutions/39039/sharing-my-simple-and-clear-c-solution/
+*/
+
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0, tempProfit = 0;
-        for (auto i = 0; i < prices.size()-1; i++)
+        int profit = 0, minPrice = pow(10,4)+1;
+        for (auto i = 0; i < prices.size(); i++)
         {
-            for (auto j = i+1; j < prices.size(); j++)
-            {
-                tempProfit = prices[j] - prices[i];
-                if(tempProfit > profit){
-                    profit = tempProfit;
-                }
-                //If prices[j+1] < prices[j], skip it
-                while(prices[j+1]<prices[j] && j < prices.size()) j++;
+            // Record lowest price up to i'th day
+            if (prices[i] < minPrice)
+                minPrice = prices[i];
+            if(prices[i]-minPrice > profit){
+                profit = prices[i] - minPrice;
             }
-            // If prices[i+1] > prices[i], skip it
-            while(prices[i+1]>prices[i] && i < prices.size()-1) i++;
+            
         }
         return profit;    
     }
@@ -30,6 +32,6 @@ int main() {
     Solution sol = Solution();
     vector<int> test1({7,1,5,3,6,4});
     vector<int> test2({7,6,4,3,1});
-    cout << sol.maxProfit(test1) << endl;
+    cout << sol.maxProfit(test2) << endl;
     return 0;
 }
