ba5126c974d874c769301a7e4e4454e91fec8f99
Dylan Lewis <dylanl826@gmail.com>
2023-11-21 16:03:40 -0600

    1st implamentation, fails on subarrays with end pointers at end of array. Stops while loop prematurely.

--- Medium/MinimumSizeSubarraySum.cpp
+++ Medium/MinimumSizeSubarraySum.cpp
@@ -1,3 +1,5 @@
+//https://leetcode.com/problems/minimum-size-subarray-sum/
+
 #include <iostream>
 #include <vector>
 
@@ -6,7 +8,23 @@ using namespace std;
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        
+        int minLength = nums.size(), curLength = 1, start = 0, end = 0, curSum = nums[start];
+        // Slidg 
+        while(end < nums.size()-1){
+            if(curSum < target){ // if under target, add end number.
+                curSum += nums[++end];
+                curLength++;
+            } else { // if at least target, record length then remove start number
+                minLength = min(minLength, curLength);
+                // Avoid start pointer from crossing end in single element subarray case.
+                if(minLength == 1){
+                    return 1;
+                }
+                curSum -= nums[start++];
+                curLength--;
+            }
+        }
+        return minLength;
     }
 };
 
@@ -15,5 +33,7 @@ int main(){
     Solution sol;
     int t1 = 7;
     vector<int> nums1{2, 2, 1, 2, 4, 3};
+
+    cout << sol.minSubArrayLen(t1, nums1);
     return 0;
 }
